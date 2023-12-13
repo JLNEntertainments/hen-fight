d512a96b164afbea00a5f5a978103644f6c338ad
SaiManiKantaBlubo <149136572+SaiManiKantaBlubo@users.noreply.github.com>
2023-12-13 11:28:22 +0530

    Added_Filling_For_Special_attack_Button-And-changed_All_the_Animation_ForButtons
    Added_Filling_For_Special_attack_Button-And-changed_All_the_Animation_ForButtons

--- Hen Fighter/Assets/Scripts/AllUiScripts/UIManager.cs
+++ Hen Fighter/Assets/Scripts/AllUiScripts/UIManager.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
     public GameObject specialAttackBtnAnim;
+
+    //For specialAttackButton Filling
+    [SerializeField] RectTransform Holder;
+    [SerializeField] Image CircleImage;
+    [SerializeField] TMP_Text TxtProgress;
+
+    [SerializeField] [Range(0, 1)] float progress = 0f;
 
     void Start()
     {
@@ -20,6 +28,12 @@ public class UIManager : MonoBehaviour
                 return;
             }
         }
+    }
+     void Update()
+    {
+        CircleImage.fillAmount = progress;
+        TxtProgress.text = Mathf.Floor(progress * 100).ToString();
+        Holder.rotation = Quaternion.Euler(new Vector3(0f, 0f, -progress * 360));
     }
 
     public void SceneChange(string ScreneName)
