5c4f122958ad494a951e35deed4cc0b7ad507164
SaiManiKantaBlubo <149136572+SaiManiKantaBlubo@users.noreply.github.com>
2023-12-14 16:55:34 +0530

    Changed_The_Character_selection_in_Hen_Ui_Screen
    Changed_The_Character_selection_in_Hen_Ui_Screen

---
+++ Hen Fighter/Assets/Scripts/AllUiScripts/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource Source;
+    public AudioClip Clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Source.PlayOneShot(Clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Hen Fighter/Assets/Scripts/AllUiScripts/GameStartCountDown.cs
+++ Hen Fighter/Assets/Scripts/AllUiScripts/GameStartCountDown.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LetsGo : MonoBehaviour
+
 {
     public GameObject CountDown;
+    
+
     void Start()
     {
         StartCoroutine ("StartDelay");
@@ -12,10 +15,11 @@ public class LetsGo : MonoBehaviour
     IEnumerator StartDelay ()
     {
         Time.timeScale = 0;
-        float pauseTime = Time.realtimeSinceStartup + 4f;
+        float pauseTime = Time.realtimeSinceStartup + 5f;
         while (Time.realtimeSinceStartup < pauseTime)
             yield return 0;
         CountDown.gameObject.SetActive(false);
         Time.timeScale = 1;
+        
     }
 }
